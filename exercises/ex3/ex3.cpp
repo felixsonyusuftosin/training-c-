c9283b55f75c04038db6708445c4e7a76e5f0bbe
Tosin Felixson-Yusuf <tosinfelixson-yusuf@Sannis-MacBook-Pro.local>
2019-01-25 16:39:35 +0100

    example 3 is done

---
+++ exercises/ex3/ex3.cpp
@@ -0,0 +1,30 @@
+// Wite a proprgram defining the memory of types in c++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Varaible of type "
+         << "bool"
+         << " takes " << sizeof(bool) << endl;
+    cout << " Varaible of type "
+         << "unsigned int"
+         << " takes " << sizeof(unsigned int) << endl;
+    cout << " Varaible of type "
+         << "int"
+         << " takes " << sizeof(int) << endl;
+    cout << " Varaible of type "
+         << "long"
+         << " takes " << sizeof(long) << endl;
+    cout << " Varaible of type "
+         << "short"
+         << " takes " << sizeof(short) << endl;
+    cout << " Varaible of type "
+         << "char"
+         << " takes " << sizeof(char) << endl;
+    cout << " Varaible of type "
+         << "double"
+         << " takes " << sizeof(double) << endl;
+}
+
